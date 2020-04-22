871acb2bf290272ca2380c9c8a7290b38c2e55b6
stegnerw <stegnerwayne@gmail.com>
2020-04-22 00:08:05 -0400

    Added tensorboard and a bunch of dense models

--- Model_Trainer.py
+++ Model_Trainer.py
@@ -11,14 +11,24 @@ import json
 import pathlib
 
 def trainModel(model, train_ds, test_ds, model_dir, verbose=2, epochs=20, shuffle=True):
+    # Create tensorboard callback
+    tensorboard_dir = model_dir.joinpath('TensorBoard')
+    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=str(tensorboard_dir), histogram_freq=1)
+
     # Fit model
-    model.fit(train_ds, validation_data=test_ds, verbose=verbose, epochs=epochs, shuffle=shuffle)
+    model.fit(train_ds, validation_data=test_ds, verbose=verbose, epochs=epochs, shuffle=shuffle, callbacks=[tensorboard_callback])
     model.evaluate(test_ds)
 
-    model.save(model_dir)
+    model.save(str(model_dir))
 
 if __name__=='__main__':
-    model_name = 'Dense_No_Hidden'
+    # Configure TF memory usage
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    if len(physical_devices) > 0:
+        config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        print('Found and configured GPU')
+    else:
+        print('Cannot find GPU')
 
     # Generate test and train datasets
     print('Creating train dataset')
@@ -48,15 +58,35 @@ if __name__=='__main__':
         shuffle_seed=shuffle_seed
     )
 
-    # Generate dense model
+    # Lists for models and model names
+    models = []
+    model_names = []
+
+    # Generate dense model with no hidden layers
+    # Set model parameters
     class_count = len(json.load(open(CLASSES_LIST)))
     hidden_layers = []
-    print('Creating model')
-    dense_model = getDenseModel(class_count, hidden_layers)
-    dense_model.summary()
+    model_name = f'Dense_0x{0:03d}'
+    # Generate model
+    print(f'Creating model {model_name}')
+    models.append(getDenseModel(class_count, hidden_layers))
+    model_names.append(model_name)
 
-    # Train model
-    model_dir = MODEL_DIR.joinpath(model_name)
-    trainModel(dense_model, train_ds, test_ds, model_dir, epochs=5)
-    print('Done')
+    # Generate dense model with hidden layers
+    for hl_count in [1, 2, 3, 4, 5]:
+        for hl_size in [32, 64, 128, 256, 512]:
+            # Set model parameters
+            class_count = len(json.load(open(CLASSES_LIST)))
+            hidden_layers = [hl_size] * hl_count
+            model_name = f'Dense_{hl_count}x{hl_size:03d}'
+            # Generate model
+            print(f'Creating model {model_name}')
+            models.append(getDenseModel(class_count, hidden_layers))
+            model_names.append(model_name)
+
+    # Train models
+    for model, model_name in zip(models, model_names):
+        model_dir = MODEL_DIR.joinpath(model_name)
+        trainModel(model, train_ds, test_ds, model_dir, epochs=1, verbose=1)
+        print(f'Done with {model_name}')
 
